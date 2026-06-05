b4060cbe04b394d8a64d02c54908f6239533c422
uchiyamazion <uchiyamazion@gmail.com>
2026-06-06 08:49:15 +0900

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,3 @@
+// ★ GASデプロイURLの設定ファイル
+// GASを再デプロイしてURLが変わった場合はここだけ更新してください
+const GAS_URL = 'https://script.google.com/macros/s/AKfycbymy6FDAsnFY-r24BNs5AlzJNBNOskCVe8D8x1ygT4As6plCkpq1rwvWZ1xR2HYSpM1Hg/exec';

6c842e6992a53107c40368975dd33a2187b925a4
uchiyamazion <uchiyamazion@gmail.com>
2026-06-08 10:15:14 +0900

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,3 +1,3 @@
 // ★ GASデプロイURLの設定ファイル
 // GASを再デプロイしてURLが変わった場合はここだけ更新してください
-const GAS_URL = 'https://script.google.com/macros/s/AKfycbwx0iOuHapkoDO73MWriIsX-54HsSkowFqQLSux1FqAn0o4N0EG-ROxvDqj1m2--mpF/exec';
+const GAS_URL = 'https://script.google.com/macros/s/AKfycbwx0iOuHapkoDO73MWriIsX-54HsSkowFqQLSux1FqAn0o4N0EG-ROxvDqj1m2--mpF/exec;

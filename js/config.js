0339e217ee040d1bea48de876a88832e791969ff
uchiyamazion <uchiyamazion@gmail.com>
2026-06-21 08:14:37 +0900

    Remove corrupted directory with newline in filename

--- js/config.js
+++
@@ -1,3 +0,0 @@
-// ★ GASデプロイURLの設定ファイル
-// GASを再デプロイしてURLが変わった場合はここだけ更新してください
-const GAS_URL = 'https://script.google.com/macros/s/AKfycbyxlntkExZheiFvTNOQRTqYbg_1JlKN4oNbo96By6U4E0MbJk7NYqNRVEfJj3gHwY7h/exec';
